a6268011f9be74237d4fee1b1be8c629ec1c96a1
Jokerye123 <jye@mail.csuchico.edu>
2021-05-06 14:14:07 -0700

    f

---
+++ Gauntlet/Assets/Scripts/DemonEnemy.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonEnemy : MonoBehaviour
+{
+    public float howFar;
+    public int speed = 5;
+    public int Hp = 5;
+
+    public GameObject player;
+    public GameObject fireball;
+    public float _fireDeathTimer;
+    public float timeBetweenShots;
+    public float startDelay;
+    public bool shooting;
+
+    Vector3 tempPos;
+    Vector3 tempRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnProjectile", startDelay, timeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, this.transform.position);
+
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+        Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1 * Time.deltaTime);
+        tempRot = transform.eulerAngles;
+
+        if (distance > howFar)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            tempPos = transform.position;
+            shooting = false;
+        }
+        else
+        {
+            shooting = true;
+        }
+        if (transform.position.y > 0 || transform.position.y < 0)
+        {
+            tempPos.y = 0;
+            transform.position = tempPos;
+
+        }
+        if (transform.rotation.y < 0 || transform.rotation.y > 0)
+        {
+            tempRot.x = 0;
+            transform.rotation = Quaternion.Euler(tempRot);
+        }
+
+        if (Hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void SpawnProjectile()
+    //similar spawner that was in the original platformer but it can now spawn enemies
+    {
+        if (shooting == true)
+        {
+           GameObject projectile = Instantiate(fireball, transform.position, fireball.transform.rotation);
+            
+           projectile.GetComponent<fireBall>().Initialize(_fireDeathTimer);
+          
+        }
+    }
+}
---
+++ Gauntlet/Assets/Scripts/fireBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBall : MonoBehaviour
+{
+    public float speed;
+    public float _deathTimer;
+    public void Initialize(float deathTimer)
+    //please comment what this does
+    {
+        _deathTimer = deathTimer;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Die());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed * Vector3.left * Time.deltaTime;
+        
+    }
+    IEnumerator Die()
+    {
+
+        yield return new WaitForSeconds(_deathTimer);
+        Destroy(this.gameObject);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+      
+            Destroy(this.gameObject);
+    }
+}
