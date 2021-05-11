84bbe5aeaf2bc787f983ae71a59d55b49f142245
Jokerye123 <jye@mail.csuchico.edu>
2021-05-11 15:52:15 -0700

    dam it

---
+++ Gauntlet/Assets/Scripts/SorcererEnemy.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SorcererEnemy : MonoBehaviour
+{
+    public bool invisible;
+    public int speed = 5;
+    public int Hp = 5;
+    public GameObject player;
+    public float timeBetween;
+    public float startDelay;
+    Vector3 tempPos;
+    Vector3 tempRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("flashing", startDelay, timeBetween);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(invisible==true)
+        {
+
+        }
+
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+        Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1 * Time.deltaTime);
+        tempRot = transform.eulerAngles;
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+        tempPos = transform.position;
+        if (transform.position.y > 0 || transform.position.y < 0)
+        {
+            tempPos.y = 0;
+            transform.position = tempPos;
+
+        }
+        if (transform.rotation.y < 0 || transform.rotation.y > 0)
+        {
+            tempRot.x = 0;
+            transform.rotation = Quaternion.Euler(tempRot);
+        }
+
+        if (Hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Player's health--;
+        }
+    }
+    public void flashing()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        if (invisible==true)
+        {
+            foreach (var r in renderers)
+            {
+                r.enabled = true;
+            }
+            invisible = false;
+        }
+        else
+        {
+            foreach (var r in renderers)
+            {
+                r.enabled = false;
+            }
+                invisible = true;
+        }
+    }
+}
